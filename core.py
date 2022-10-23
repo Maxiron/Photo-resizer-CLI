408f1bc698d78cf402e3f7da9484658ec8720820
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-10-23 22:30:07 +0100

    core_skeleton_done

---
+++ core.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from pathlib import Path
+
+
+def fav_generator(in_path, icon_size):   
+       icon_sizes = [(icon_size, icon_size)]
+       image = Image.open(image_url)
+       img_format = image.format
+       for size in icon_sizes:
+              new_image = image.resize(size)
+              new_image.save(f"{Path(in_path).stem}_{size[0]}x{size[1]}.{img_format}")
+
+image_url, icon_size = 'pubg.jpg', 30
+
+fav_generator(image_url, icon_size)
