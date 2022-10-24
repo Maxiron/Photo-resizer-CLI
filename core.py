3a1077754888de85abe08ee62296379bff32ce03
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-10-24 12:24:22 +0100

    fix

--- core.py
+++ core.py
@@ -10,6 +10,6 @@ def fav_generator(in_path, icon_size):
               new_image = image.resize(size)
               new_image.save(f"{Path(in_path).stem}_{size[0]}x{size[1]}.{img_format}")
 
-image_url, icon_size = 'pubg.jpg', 30
+# image_url, icon_size = 'pubg.jpg', 30
 
-fav_generator(image_url, icon_size)
+# fav_generator(image_url, icon_size)
